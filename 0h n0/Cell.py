a12e4a97aed0d7cedeff0298d1a0e54d1825e382
Craner <iayfnp@gmail.com>
2022-12-11 01:20:17 -0800

    changing to only appending number cells to cardinal directions

--- 0h n0/Cell.py
+++ 0h n0/Cell.py
@@ -1,10 +1,111 @@
+import numpy as np
+
 class Cell:
+    board = np.full([9, 9], None)
+    PostPredict = []
+
     def __init__(self, x, y, state = None):
         self.x = x
         self.y = y
         self.state = state
-  
-    def __str__(self):
-        return self.state
+        self.left = self.right = self.top = self.bottom = None
 
+    def initRed(self):# switch to mark() function
+        # change to item.predict()
+        temp = None
+        for item in Cell.board[self.y][:self.x:-1]:
+            if item.state == -1:
+                break
+            if item.state != 0:
+                item.initBlue(temp, self.x, self.y)
+                temp = item
+        for item in Cell.board[self.y][self.x + 1:]:
+            if item.state == -1:
+                break
+            if item.state != 0:
+                item.initBlue(self.x, self.y)
+        for item in Cell.board[:self.y:-1,self.x]:
+            if item.state == -1:
+                break
+            if item.state != 0:
+                item.initBlue(self.x, self.y)
+        for item in Cell.board[self.y + 1:,self.x]:
+            if item.state == -1:
+                break
+            if item.state != 0:
+                item.initBlue(self.x, self.y)
+
+    def initBlue(self, x = None, y = None):
+        self.left = []
+        self.right = []
+        self.top = []
+        self.bottom = []
+        # remove?
+        for item in Cell.board[self.y][:self.x:-1]:
+            if item.state == -1:
+                break
+            if item.state > 0:
+                self.left.append(item)
+        for item in Cell.board[self.y][self.x + 1:]:
+            if item.state == -1:
+                break
+            if item.state > 0:
+                self.right.append(item)
+        for item in Cell.board[:self.y:-1,self.x]:
+            if item.state == -1:
+                break
+            if item.state > 0:
+                self.top.append(item)
+        for item in Cell.board[self.y + 1:,self.x]:
+            if item.state == -1:
+                break
+            if item.state > 0:
+                self.bottom.append(item)
+
+        if x:
+            if x < self.x:
+                self.left = Cell.board[self.y][x + 1:self.x]
+                self.left = temp.left + temp
+            else:
+                self.right = Cell.board[self.y][self.x + 1:x]
+        else:
+            if y < self.y:
+                self.top = Cell.board[y + 1:self.y,self.x]
+            else:
+                self.bottom = Cell.board[self.y + 1:y,self.x]
+
+    def hi(self):
+        if not self.left:
+            self.left = [item for item in Cell.board[self.y][:self.x] if item.state != 0]
+            self.left = Cell.board[self.y][:self.x]
+        if not self.right:
+            self.right = Cell.board[self.y][self.x + 1:]
+        if not self.top:
+            self.top = Cell.board[:self.y,self.x]
+        if not self.bottom:
+            self.bottom = Cell.board[self.y + 1:,self.x]
+
+    def __str__(self):
+        return str(self.state)
+
+    def markRed(self):
+        self.state = -1
+        # switch to mark() function
+        # init red but change to item.predict()
+
+    def markBlue(self):
+        self.state = 0
+
+    def predict(self):
+        if not self.left:
+            self.left = Cell.board[self.y][:self.x]
+        if not self.right:
+            self.right = Cell.board[self.y][self.x + 1:]
+        if not self.top:
+            self.top = Cell.board[:self.y,self.x]
+        if not self.bottom:
+            self.bottom = Cell.board[self.y + 1:,self.x]
+        return
     
+    def print():
+        print(np.array([[elem.state if elem.state else 0 for elem in item] for item in Cell.board]))
--- 0h n0/_0h_n0.py
+++ 0h n0/_0h_n0.py
@@ -3,31 +3,76 @@ import numpy as np
 from PIL import Image
 from Cell import Cell
 
+"""
+Cell [0, 0] dimensions
+    Left value: 678
+    Right value: 731
+    Top value: 275
+    Bottom value: 328
+    Diameter: 53
+Cell [8, 8] dimensions
+    Left value: 1151
+    Right value: 1204
+    Top value: 749
+    Bottom value: 802
+    Diameter: 53
+Together
+    [941, 538.5]
+    [941, 538]
+    # <= 0.5: round down
+    # > 0.5: round up
+    X: (1204 - 731) / 8 = (1151 - 678) / 8 = 59.125
+    Y: (802 - 328) / 8 = (749 - 275) / 8 = 59.25
+"""
+
 #print("Press 'ENTER' to start the program")
 #input()
 
 X = []
 Y = []
-board = np.zeros([9, 9])
-
 for index in range(9):
-    X.append(58.75 * index + 705)
-    Y.append(59.375 * index + 300)
+    X.append(round(59.125 * index + 704.4))
+    Y.append(round(59.25 * index + 301.4))
 
-girth = 25
+length = 25
+shifts = []
+left = X[0] - length / 2
+top = Y[0] - length / 2
 screen = Image.open('screen.png')
-# screen = pyautogui.screenshot('screen.png')
-# pyautogui.screenshot('The One.png', region = (942 - girth / 2, 539 - girth / 2, girth, girth))
+# screen = pyautogui.screenshot('screen.png', region = (470, 475))
+# pyautogui.screenshot('The One.png', region = (942 - length / 2, 539 - length / 2, length, length))
+#board = np.full([9, 9], None, Cell)
+
+reds = []
+blues = []
 for itY in range(9):
     for itX in range(9):
         pixel = screen.getpixel((X[itX], Y[itY]))
         if pixel == (238, 238, 238):
+            Cell.board[itY][itX] = Cell(itX, itY)
             continue
         elif pixel == (255, 56, 75):
+            Cell.board[itY][itX] = Cell(itX, itY, -1)
+            reds.append(Cell.board[itY][itX])
             continue
         for number in range(1, 10):
-            if pyautogui.locate(str(number) + '.png', 'screen.png', region = (int(X[itX] - girth / 2), int(Y[itY] - girth / 2), girth, girth), confidence = 0.85):
-                board[itY][itX] = number
-print(board)
+            if pyautogui.locate(str(number) + '.png', screen, region = (int(X[itX] - length / 2), int(Y[itY] - length / 2), length, length), confidence = 0.85):
+                Cell.board[itY][itX] = Cell(itX, itY, number)
+                blues.append(Cell.board[itY][itX])
+
+x = 1
+y = 2
+Cell.print()
+
+print([item.state for item in Cell.board[:,x]])
+print([item.state for item in Cell.board[y:9,x]])
+for cell in reds:
+    cell.initRed()
+for cell in blues:
+    cell.initBlue()
+
+for cell in blues:
+    cell.predict()
+Cell.print()
 #pyautogui.click(100, 200)
 #pyautogui.doubleClick(100, 200)
