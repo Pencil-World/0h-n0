473eba5fdc475e21bc31b406a8237e0acc635e01
Craner <iayfnp@gmail.com>
2023-01-14 17:49:51 -0800

    massive changes

--- 0h n0/Cell.py
+++ 0h n0/Cell.py
@@ -1,159 +1,225 @@
-import numpy as np
-
-
 """
 -10 is blank
 -1 is red
 0 is blue
 1-9 is number
+r is red/locked
+g is gray/blank
 """
 
 class Cell:
-    board = np.full([9, 9], None) # (y, x)
-    predictions = {} # unnecessary
-    UpdateCell = None # its important to keep track of last updated cell becuase the predicted cells in the following might not be predicted. each numbered cell has a range
-    # update cell is redundant because pass self to predict functions
+    board = np.full([9, 9], None) # (y, x). read left to right, top to bottom. 
+    predictions = {}
 
-    # read left to right, top to bottom
-    def __init__(self, x, y, state = None):
+    def __init__(self, x, y, state):
         self.x = x
         self.y = y
-        self.state = state
-        self.leftVal = self.rightVal = self.topVal = self.bottomVal = 0
-        self.left, self.right, self.top, self.bottom = [[], [], [], []]
-        AdjacentLeft = Cell.board[self.x - 1][self.y]
-        AdjacentTop = Cell.board[self.x][self.y - 1]
-        
-        if self.state != -1:
-            if self.x > 0:
-                self.right = AdjacentLeft.right
-                self.left = [AdjacentLeft] + AdjacentLeft.left if AdjacentLeft.state > 0 else AdjacentLeft.left
-            if self.y > 0:
-                self.bottom = AdjacentTop.bottom
-                self.top = [AdjacentTop] + AdjacentTop.top if AdjacentTop.state > 0 else AdjacentTop.top
+        self.freedom = self.state = state
+        self.left, self.right, self.top, self.bottom = [], [], [], []
+        self.horzLim, self.vertLim = [[[10]], [[10]]] # references. can connect horzLim of clusters through one. can change all horzLim of a cluster through one
 
+        # link cells together and lets them view numbered cells in the cardinal directions
+        if self.state != -1:
+            AdjacentLeft = Cell(0, 0, -1) if self.x == 0 else Cell.board[self.y][self.x - 1]
+            if AdjacentLeft.state < 0:
+                self.right = AdjacentLeft.right
             if self.state > 0:
-                if self.x > 0:
-                    AdjacentLeft.right.append(self)
-                if self.y > 0:
-                    AdjacentTop.bottom.append(self)
+                self.left = [self] + AdjacentLeft.left
+                for elem in self.left:
+                    elem.right.append(self)
+            else:
+                self.left = AdjacentLeft.left
+
+            AdjacentTop = Cell(0, 0, -1) if self.y == 0 else Cell.board[self.y - 1][self.x]
+            if AdjacentTop.state < 0:
+                self.bottom = AdjacentTop.bottom
+            if self.state > 0:
+                self.top = [self] + AdjacentTop.top
+                for elem in self.top:
+                    elem.bottom.append(self)
+            else:
+                self.top = AdjacentTop.top
 
     def __repr__(self):
-        return str(self.state if self.state else 0)
+        return str(self.state) if self.state >= 0 else ('r' if self.state == -1 else 'g')
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def printActive(self):
+        print()
+        print(f"(Y, X): ({self.y}, {self.x}), state: {self.state}, freedom: {self.freedom}")
+        print(f"horzLim: {self.horzLim}, vertLim: {self.vertLim}")
+        print(f"leftVal: {self.leftVal}, {[elem.x for elem in self.LEFT]}")
+        print(f"rightVal: {self.rightVal}, {[elem.x for elem in self.RIGHT]}")
+        print(f"topVal: {self.topVal}, {[elem.y for elem in self.TOP]}")
+        print(f"bottomVal: {self.bottomVal}, {[elem.y for elem in self.BOTTOM]}")
+
+    def printInactive(self):
+        print()
+        print(f"(Y, X): ({self.y}, {self.x}), state: {self.state}")
+        print(f"left: {self.left}")
+        print(f"right: {self.right}")
+        print(f"top: {self.top}")
+        print(f"bottom: {self.bottom}")
 
     def init(self):
+        for numbered cells only. create slice copy
+        self.left = self.left[1:]
         self.right = self.right[:-1]
+        self.top = self.top[1:]
         self.bottom = self.bottom[:-1]
-        self.left, self.right, self.top, self.bottom = [Cell.board[self.y][self.x:self.horzLim - self.leftVal:-1], 
-                                                        Cell.board[self.y][self.x + 1:self.horzLim - self.rightVal], 
-                                                        Cell.board[self.x:self.horzLim - self.leftVal:-1][self.y], # fix
-                                                        Cell.board[self.x:self.horzLim - self.leftVal:][self.y]]
 
-        for arr in [self.left, self.right, self.top, self.bottom]:
-            for i, cell in enumerate(arr):
-                if cell.state > 0 and cell.state < len(arr):
-                    del arr[i - 1:]
+        # replace the funcionalities of self.left with self.LEFT. so self.left, self.right, self.top, self.bottom = [,,,,]
+        self.LEFT, self.RIGHT, self.TOP, self.BOTTOM = [Cell.board[self.y][max(0, self.x - (self.leftVal + self.horzLim)):self.x][::-1].tolist(), 
+                                                        Cell.board[self.y][self.x + 1:self.x + 1 + (self.rightVal + self.horzLim)].tolist(), 
+                                                        Cell.board[max(0, self.y - (self.topVal + self.vertLim)):self.y,self.x][::-1].tolist(), 
+                                                        Cell.board[self.y + 1:self.y + 1 + (self.bottomVal + self.vertLim),self.x].tolist()]
+
+        # slice list when reaching a barrier or when overstepping
+        for arr in [self.LEFT, self.RIGHT, self.TOP, self.BOTTOM]:
+            for i, elem in enumerate(arr):
+                if -1 <= elem.state <= i:
+                    del arr[i - (elem.state != -1):]
                     break
 
-    def update(self, direction = None):
-        # affect cells which it can "see" and predicts for them
-        # test if references or not to prevent overcopying
-        mat = None
-        match direction:
-            case "left" | "right":
-                mat = [self.top, self.bottom]
-            case "top" | "bottom":
-                mat = [self.left, self.right]
-            case other:
-                mat = [self.left[0].left, self.right[0].right, self.top[0].top, self.bottom[self.bottomVal].bottom] # hmm not sure what to do here. have left and LEFT because what if there are continuouse number slots, we need to account for those
+    def trim(self):
+        for arr in [self.LEFT, self.RIGHT, self.TOP, self.BOTTOM]:
+            for i, elem in enumerate(arr):
+                if -1 <= elem.state <= i:
+                    del arr[i - (elem.state != -1):]
+                    break
+        return
 
-        for arr in mat:
-            for cell in arr:
-                cell.predict(self)
+    # affect cells which it can "see" and predicts for them
+    # key-value pairs: {0: "left", 1: "right", 2: "top", 3: "bottom"}
+    def update(self):
+        # match direction:
+        #     case "left" | "right":
+        #         mat = [self.top, self.bottom]
+        #     case "top" | "bottom":
+        #         mat = [self.left, self.right]
+        #     case other:
+        #         mat = [self.LEFT, self.RIGHT, self.TOP, self.BOTTOM]
+        # if direction == "0" or direction == "1":
+        #     mat = [self.top, self.bottom]
+        # elif direction == "2" or direction == "3":
+        #     mat = [self.left, self.right]
+        # else:
+        #     mat = [self.left, self.right, self.top, self.bottom]
 
-    def markRed(self, direction):
-        self.state = -1
-        update(direction)
+        for arr in [self.left, self.right, self.top, self.bottom]:
+            for elem in arr:
+                Cell.board
+                elem.predict()
 
-    def markBlue(self, direction):
-        self.state = 0
-        update(direction)
+    def markRed(self):
+        if self.state == -10:
+            self.state = -1
+            update()
 
-    # Each numbered cell has 24 permuations for the order of filling out the 4 cardinal directions
-    # This number can see all its dots
-    # One specific dot is included in all solutions imaginable
-    # Only one direction remains for this number to look in
-    # Looking further in one direction would exceed this number
-    def predict(self, cell):
-        # update state/leftVal/topVal
-        # update left and right to check of overstepping
-        if cell.x > self.x - len(self.left):
-            return
-        if cell.x == self.x - leftVal:
-            return
+    def markBlue(self):
+        if self.state == -10:
+            self.state = 0
+            update()
 
-        freedom = self.state - (self.leftVal + self.rightVal + self.topVal + self.bottomVal) # set in init? or in main?
-        if freedom == 0:
-            # This number can see all its dots
-            self.left[self.leftVal].markRed("left")
-            self.right[self.rightVal].markRed("right")
-            self.top[self.topVal].markRed("top")
-            self.bottom[self.bottomVal].markRed("bottom")
-            self.update()
-        else:
+    # source cell may be out of range of the target cell because it connects to a cluster which interferes by possibly creating overstepping for the target cell
+    def WithinRange(self, cell):
+        # index = [self.x - self.leftVal < cell.x < self.x, self.x < cell.x < self.x + self.rightVal, 
+        #          self.y - self.topVal < cell.y < self.y, self.y < cell.y < self.y + self.bottomVal].index(True) # remove this check
+        # if index == -1:
+        #     return -1
+        index = [cell.x < self.x, self.x < cell.x, cell.y < self.y, self.y < cell.y].index(True)
+
+        # mat = [self.LEFT, self.RIGHT, self.TOP, self.BOTTOM]
+        # for arr in mat:
+        #     for i, elem in enumerate(arr[::-1]):
+        #         if elem.state == -1:
+        #             del arr[-i:]
+
+        WithinRange = False
+        [elem.state for elem in self.left].index(-1)
+        if cell.state == -1:
+            mat = [self.LEFT[self.x - cell.x:], self.RIGHT[cell.x - self.x:], self.TOP[self.y - cell.y:], self.BOTTOM[cell.y - self.y:]]
+            if mat[index] != []:
+                WithinRange = True
+                del mat[index]
+        elif cell.state == 0:
+            mat = [self.LEFT[self.leftVal:self.leftVal + self.freedom], self.RIGHT[self.rightVal:self.freedom], self.TOP[self.topVal:self.freedom], self.BOTTOM[self.bottomVal:self.freedom]]
+            for count, elem in enumerate(mat[index]):
+                if elem.state < 0:
+                    break
+            if count:
+                WithinRange = True
+                self.freedom -= count
+                # match index:
+                #     case 0:
+                #         self.leftVal += count
+                #     case 1:
+                #         self.rightVal += count
+                #     case 2: 
+                #         self.topVal += count
+                #     case 3: 
+                #         self.bottomVal += count
+                if index == 0:
+                    self.leftVal += count
+                elif index == 1:
+                    self.rightVal += count
+                elif index == 2:
+                    self.topVal += count
+                elif index == 3:
+                    self.bottomVal += count
+
+                # reduce self.LEFT when it oversteps by connecting to a secondary cluster
+                # is this unnecessary? let's try without it first. 
+                # for arr in [self.LEFT[self.leftVal + self.freedom::-1], self.RIGHT[self.rightVal + self.freedom::-1], 
+                #             self.TOP[self.topVal + self.freedom::-1], self.BOTTOM[self.bottomVal + self.freedom::-1]]:
+                #     for i, elem in enumerate(arr):
+                #         if elem < 0:
+                #             del arr[i:]
+                #             break
+
+        return index if WithinRange else -1
+
+    # create a direction function which directly passes the index. 
+    def predict(self):
+        # Each numbered cell has 24 permuations for the order of completing the 4 cardinal directions
+        direction = WithinRange()
+        # loop this until not WithinRange, or for a better description: no change to freedom or left/right/top/bottom states? 
+        if direction != -1:
             # One specific dot is included in all solutions imaginable
             # Only one direction remains for this number to look in
-            diff = len(self.left) + len(self.right) + len(self.top) + len(self.bottom) - self.state
-            if self.leftValue < diff:
-                for index, elem in enumerate(self.left[self.leftVal:]):
-                    if index < diff:
-                        elem.markBlue()
-                    elif elem.state >= 0:
-                        self.state += 1
-                    else:# state is -1 or None
+            diff = len(self.LEFT) + len(self.RIGHT) + len(self.TOP) + len(self.BOTTOM) - self.state
+            mat = [self.LEFT[self.leftVal:-diff], self.RIGHT[self.rightVal:-diff], self.TOP[self.topVal:-diff], self.BOTTOM[self.bottomVal:-diff]]
+            for arr in [self.LEFT[self.leftVal:], self.RIGHT[self.rightVal:-diff], self.TOP[self.topVal:-diff], self.BOTTOM[self.bottomVal:-diff]]:
+                for count, elem in enumerate(arr):
+                    if elem.state > 0 and count < len(arr) - diff:
                         break
-            self.state -= diff - self.leftVal # important
+                    elem.markBlue() # does marking blue connect to secondary clusters? prep current cell for if it can see all its dots from connecting to secondary clusters
 
-            if self.state < self.left[0].leftValue:
-                # check all directions to see if the cell will over step. if it does, then markRed()
-                self.left[0].markRed()
-            if self.leftValue < diff:
-                for index, elem in enumerate(self.left[self.leftVal:]):
-                    if index < diff:
-                        elem.markBlue()
-                    elif elem.state >= 0:
-                        self.state += 1
-                    else:# state is -1 or None
-                        break
-            if self.state < self.left[0].leftValue:
-                # check all directions to see if the cell will over step. if it does, then markRed()
-                self.left[0].markRed()
-            if self.leftValue < diff:
-                for index, elem in enumerate(self.left[self.leftVal:]):
-                    if index < diff:
-                        elem.markBlue()
-                    elif elem.state >= 0:
-                        self.state += 1
-                    else:# state is -1 or None
-                        break
-            if self.state < self.left[0].leftValue:
-                # check all directions to see if the cell will over step. if it does, then markRed()
-                self.left[0].markRed()
-            if self.leftValue < diff:
-                for index, elem in enumerate(self.left[self.leftVal:]):
-                    if index < diff:
-                        elem.markBlue()
-                    elif elem.state >= 0:
-                        self.state += 1
-                    else:# state is -1 or None
-                        break
+                self.freedom -= count
+                if direction == 0:
+                    self.leftVal += count
+                    update = self.LEFT[self.leftVal]
+                elif direction == 1:
+                    self.rightVal += count
+                elif direction == 2:
+                    self.topVal += count
+                elif direction == 3:
+                    self.bottomVal += count
 
+            # This number can see all its dots
             # Looking further in one direction would exceed this number
-            if sum(elem.state > 0 for elem in self.left[self.leftVal + 1:freedom + self.leftVal + 1] == freedom:
-                self.left[self.leftVal].markRed("left")
-            if sum(elem.state > 0 for elem in self.left[self.leftVal:freedom + 1]) == freedom:
-                self.left[self.leftVal].markRed("left")
-            if sum(elem.state > 0 for elem in self.left[self.leftVal:freedom + 1]) == freedom:
-                self.left[self.leftVal].markRed("left")
-            if sum(elem.state > 0 for elem in self.left[self.leftVal:freedom + 1]) == freedom:
-                self.left[self.leftVal].markRed("left")
+            if sum([elem.state >= 0 for elem in Cell.board[self.leftVal + 1:self.freedom + self.leftVal + 1:-1]]) == self.freedom:
+                self.left[self.leftVal].markRed(0)
+            if sum([elem.state >= 0 for elem in Cell.board[self.rightVal + 1:self.freedom + self.rightVal  + 1]]) == self.freedom:
+                self.left[self.leftVal].markRed(1)
+            if sum([elem.state >= 0 for elem in self.TOP[self.topVal + 1:self.freedom + self.topVal + 1]]) == self.freedom:
+                self.left[self.leftVal].markRed(2)
+            if sum([elem.state >= 0 for elem in self.BOTTOM[self.bottomVal + 1:self.freedom + self.bottomVal + 1]]) == self.freedom:
+                self.left[self.leftVal].markRed(3)
+
+            Cell.predictions.discard(self)
--- 0h n0/_0h_n0.py
+++ 0h n0/_0h_n0.py
@@ -1,10 +1,3 @@
-import pyautogui
-import numpy as np
-from PIL import Image
-from Cell import Cell
-import keyboard
-import time
-
 """
 Cell [0, 0] dimensions
     Left value: 678
@@ -27,124 +20,116 @@ Together
     Y: (802 - 328) / 8 = (749 - 275) / 8 = 59.25
 """
 
-#print("Press 'ENTER' to start the program")
-#keyboard.wait('ENTER')
-
 # unit testing here
 
+def CreateGrid():
+    print("CreateGrid")
+    for index in range(9):
+        X.append(round(59.125 * index + 704.4))
+        Y.append(round(59.25 * index + 301.4))
+    
+    print(X)
+    print(Y)
+
+def CreateBoard():
+    print("CreateBoard")
+    for y in range(0, 9):
+        for x in range(0, 9):
+            val = board[y][x]
+            Cell.board[y][x] = Cell(x, y, val if val else -10)
+            if val > 0:
+                NumberedCells.append(Cell.board[y][x])
+    print(Cell.board)
+
+    # Cell.board[7][2].printInactive()
+    # Cell.board[5][0].printInactive()
+    # Cell.board[0][4].printInactive()
+    # Cell.board[6][8].printInactive()
+    # Cell.board[8][4].printInactive()
+
 def horzClustering():
+    print("horzClustering")
     for y in range(0, 9):
         count = 0
-        for x in range(0, 9):
-            if Cell.board[y][x].state > 0:
+        for x in range(0, 10):
+            if x != 9 and Cell.board[y][x].state > 0:
                 count += 1
             elif count:
                 arr = Cell.board[y][x - count:x]
-                horzLim = min(arr)
+                horzLim = min([elem.freedom - (count - 1) for elem in arr])
                 for i, elem in enumerate(arr):
-                    elem.leftVal += x
-                    elem.rightVal += count - x
-                    elem.freedom -= count
+                    elem.leftVal = i
+                    elem.rightVal = count - 1 - i
+                    elem.freedom -= count - 1
                     elem.horzLim = horzLim
                 count = 0
+    print(np.array([[elem.horzLim if hasattr(elem, 'horzLim') else 0 for elem in arr] for arr in Cell.board]))
 
 def vertClustering():
+    print("vertClustering")
     for x in range(0, 9):
         count = 0
-        for y in range(0, 9):
-            if Cell.board[y][x].state > 0:
+        for y in range(0, 10):
+            if y != 9 and Cell.board[y][x].state > 0:
                 count += 1
             elif count:
                 arr = Cell.board[y - count:y:,x]
-                vertLim = min(arr)
+                vertLim = min([elem.freedom - (count - 1) for elem in arr])
                 for i, elem in enumerate(arr):
-                    elem.topVal += x
-                    elem.bottomVal += count - x
-                    elem.freedom -= count
+                    elem.topVal = i
+                    elem.bottomVal = count - 1 - i
+                    elem.freedom -= count - 1
                     elem.vertLim = vertLim
                 count = 0
+    print(np.array([[elem.vertLim if hasattr(elem, 'vertLim') else 0 for elem in arr] for arr in Cell.board]))
 
-X = []
-Y = []
-for index in range(9):
-    X.append(round(59.125 * index + 704.4))
-    Y.append(round(59.25 * index + 301.4))
+def ActivateNumberedCells():
+    print("ActivateNumberedCells")
+    # NumberedCells[6].init() # primarily 0 and 6
+    for cell in NumberedCells:
+        cell.init()
+
+    # Cell.board[1][3].printActive()
+    # Cell.board[0][5].printActive()
+    # Cell.board[4][0].printActive()
+    # Cell.board[8][8].printActive()
+
+def solve():
+    for cell in NumberedCells:
+        cell.predict()
+    print(Cell.board)
+
+X, Y = [], []
+CreateGrid()
 
 width = 25
 shifts = [] #advanced pixel comparisons
-reds = []
 blues = []
-coord = [0, -1]
-screen = Image.open('screen.png')
-keyboard.wait('ENTER')
+# screen = Image.open('screen.png')
 
+#print("Press 'ENTER' to start the program")
+#keyboard.wait('ENTER')
 # screen = pyautogui.screenshot('screen.png', region = (470, 475))
 # pyautogui.screenshot('The One.png', region = (942 - width / 2, 539 - width / 2, width, width))
-board = [[10, 10, 10, 10, 10, 6, 9, -1, 10], 
-         [5, 10, 10, 6, 10, 10, 10, 3, 10], 
-         [5, 10, 10, 10, -1, 10, 10, 10, 10], 
-         [-1, 10, 10, 7, 10, 10, 10, 10, 10], 
-         [4, 10, -1, 10, 2, 10, 10, 10, 5], 
-         [10, -1, 3, 10, 10, 5, 10, 10, -1], 
-         [10, 10, 4, 10, -1, 10, 2, 10, 10], 
-         [10, 7, 10, 10, 10, 6, 10, 5, 10], 
-         [10, 3, 10, 10, 10, 5, 10, 10, 5]]
 
-for y in range(0, 9):
-    for x in range(0, 9):
-        val = board[y][x]
-        if val == 10:
-            Cell.board[y][x] = Cell(x, y)
-        else:
-            Cell.board[y][x] = Cell(x, y, None if board[y][x] == 10 else board[y][x])
-            if val > 0:
-                blues.append(Cell.board[itY][itX])
+NumberedCells = []
+board = [[0, 0, 0, 0, 0, 6, 9, -1,0], 
+         [5, 0, 0, 6, 0, 0, 0, 3, 0], 
+         [5, 0, 0, 0, -1,0, 0, 0, 0], 
+         [-1,0, 0, 7, 0, 0, 0, 0, 0], 
+         [4, 0, -1,0, 2, 0, 0, 0, 5], 
+         [0, -1,3, 0, 0, 5, 0, 0, -1], 
+         [0, 0, 4, 0, -1,0, 2, 0, 0], 
+         [0, 7, 0, 0, 0, 6, 0, 5, 0], 
+         [0, 3, 0, 0, 0, 5, 0, 0, 5]]
+print(board)
 
+CreateBoard()
 horzClustering()
 vertClustering()
+print(np.array([[0 if elem.freedom == -10 else elem.freedom for elem in arr] for arr in Cell.board]))
+ActivateNumberedCells()
+solve()
 
-#for width in range(8, -1, -1):
-#    for it in range(1, -1, -1):
-#        for temp in range(width + it):
-#            coord[it] += -1 if width % 2 else 1
-#            itX, itY = coord[1], coord[0]
-#            if board[itX][itY] == 10:
-#                Cell.board[itY][itX] = Cell(itX, itY)
-#            else:
-#                Cell.board[itY][itX] = Cell(itX, itY, board[itX][itY])
-#                if board[itX][itY] == -1:
-#                    reds.append(Cell.board[itY][itX])
-#                else:
-#                    blues.append(Cell.board[itY][itX])
-#            Cell.board[itY][itX].init() # remove?
-
-#for width in range(8, -1, -1):
-#    for it in range(1, -1, -1):
-#        for temp in range(width + it):
-#            coord[it] += -1 if width % 2 else 1
-#            itX, itY = coord[1], coord[0]
-#            pixel = screen.getpixel((X[itX], Y[itY]))
-
-#            if pixel == (238, 238, 238):
-#                Cell.board[itY][itX] = Cell(itX, itY)
-#            elif pixel == (255, 56, 75):
-#                Cell.board[itY][itX] = Cell(itX, itY, -1)
-#                reds.append(Cell.board[itY][itX])
-#            else:
-#                for number in range(1, 10):
-#                    if pyautogui.locate(str(number) + '.png', screen, region = (X[itX] - width // 2, Y[itY] - width // 2, width, width), confidence = 0.85):
-#                        Cell.board[itY][itX] = Cell(itX, itY, number)
-#                        blues.append(Cell.board[itY][itX])
-#print([elem.state for elem in Cell.board[:,x]])
-#print([elem.state for elem in Cell.board[y:9,x]])
-
-print(Cell.board)
-for cell in blues:
-    cell.init()
-
-for cell in blues:
-    cell.predict()
-print(Cell.board)
 #pyautogui.click(100, 200)
 #pyautogui.doubleClick(100, 200)
-time.sleep(5)
